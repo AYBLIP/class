4dfe1a71e151364ec3f509c46bb2fe24e494dccd
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-18 02:12:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from tensorflow.keras.applications.efficientnet import preprocess_input
 
-# Muat model yang sudah dilatih
+# Muat model yang sudah dilatih (pastikan model ini sudah dilatih dan menyimpan arsitektur + bobot)
 model = tf.keras.models.load_model('model_Adam.h5')
 
 # Daftar label kelas
@@ -18,7 +19,7 @@ kelas = [
     'Kue Apem'
 ]
 
-st.title("Aplikasi Klasifikasi Citra Digital Kue")
+st.title("Aplikasi Klasifikasi Citra Digital Kue dengan EfficientNetB0")
 st.write("Unggah gambar kue untuk diklasifikasikan ke dalam salah satu dari 8 kelas.")
 
 # Upload gambar
@@ -26,17 +27,23 @@ uploaded_file = st.file_uploader("Pilih gambar", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     # Baca gambar
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert('RGB')
     st.image(image, caption='Gambar yang diunggah', use_column_width=True)
     
-    # Preprocessing gambar sesuai model
-    # Misalkan model membutuhkan input 128x128
-    image_resized = image.resize((128, 128))
-    img_array = np.array(image_resized) / 255.0  # normalisasi
-    img_array = np.expand_dims(img_array, axis=0)  # bentuk (1, 128, 128, 3)
+    # Resize sesuai input model (misalnya 224x224 untuk EfficientNetB0)
+    image_resized = image.resize((224, 224))
+    
+    # Konversi ke array numpy
+    img_array = np.array(image_resized)
+    
+    # Preprocessing sesuai EfficientNetB0
+    img_preprocessed = preprocess_input(img_array)
+    
+    # Tambahkan batch dimension
+    img_input = np.expand_dims(img_preprocessed, axis=0)
     
     # Prediksi
-    pred = model.predict(img_array)
+    pred = model.predict(img_input)
     pred_kelas = np.argmax(pred, axis=1)[0]
     confidence = pred[0][pred_kelas]
     
