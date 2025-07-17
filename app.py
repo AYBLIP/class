931be37776576eb966aa009745658a7345203eaf
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-18 02:29:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,50 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import os
 
-# Load model yang sudah dilatih
+# Load models
 @st.cache(allow_output_mutation=True)
 def load_models():
-    model_adam = load_model('model_klasifikasi_kue.h5')
-    return {
-        'Adam': model_adam,
-        'SGD': model_sgd,
-        'RMSprop': model_rmsprop
+    models = {
+        'Adam': load_model('models/model_Adam.h5'),
+        'SGD': load_model('models/model_SGD.h5'),
+        'RMSprop': load_model('models/model_RMSprop.h5')
     }
+    return models
 
-model_dict = load_models()
+models = load_models()
 
-# Judul Aplikasi
-st.title('Klasifikasi Kue Indonesia')
+st.title("Klasifikasi Kue menggunakan EfficientNetB0")
 
-# Pilih optimizer yang ingin digunakan
-optimizer_choice = st.selectbox('Pilih optimizer untuk prediksi:', ['Adam'])
+# Pilih model
+model_choice = st.selectbox("Pilih model untuk prediksi:", list(models.keys()))
+model = models[model_choice]
 
 # Upload gambar
-uploaded_file = st.file_uploader("Unggah gambar kue", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload gambar kue", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Tampilkan gambar yang diunggah
+    # Tampilkan gambar
     image = load_img(uploaded_file, target_size=(224, 224))
-    st.image(image, caption='Gambar Unggahan', use_column_width=True)
+    st.image(image, caption='Gambar yang diupload', use_column_width=True)
 
-    # Konversi gambar ke array dan preprocess
-    img_array = img_to_array(image) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    # Proses prediksi
+    if st.button("Prediksi"):
+        img_array = img_to_array(image) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
 
-    # Prediksi
-    model = model_dict[optimizer_choice]
-    pred_probs = model.predict(img_array)
-    pred_class = np.argmax(pred_probs, axis=1)[0]
+        pred_probs = model.predict(img_array)
+        pred_class = np.argmax(pred_probs, axis=1)[0]
 
-    # Tampilkan hasil prediksi
-    class_labels = list(test_generator.class_indices.keys())  # Pastikan ini sesuai dengan data
-    predicted_label = class_labels[pred_class]
-    confidence = pred_probs[0][pred_class]
+        class_indices = {v: k for k, v in train_generator.class_indices.items()}
+        # Pastikan class_indices sesuai dengan label yang digunakan saat training
+        # Jika tidak, buat dictionary manual sesuai label
+        class_labels = list(train_generator.class_indices.keys())
 
-    st.write(f'Prediksi: **{predicted_label}** dengan kepercayaan {confidence:.2f}')
+        predicted_label = class_labels[pred_class]
+        confidence = pred_probs[0][pred_class]
+
+        st.write(f"Prediksi: **{predicted_label}** dengan kepercayaan {confidence*100:.2f}%")
