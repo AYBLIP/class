e5153ac05d35f14303e9c232ed7aaa990b234ba4
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-18 02:15:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,50 @@
 import streamlit as st
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
 import numpy as np
-from PIL import Image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.utils import get_custom_objects
+import os
 
-# Daftarkan fungsi swish
-def swish(x):
-    return x * tf.keras.backend.sigmoid(x)
+# Load model yang sudah dilatih
+@st.cache(allow_output_mutation=True)
+def load_models():
+    model_adam = load_model('model_Adam.h5')
+    model_sgd = load_model('model_SGD.h5')
+    model_rmsprop = load_model('model_RMSprop.h5')
+    return {
+        'Adam': model_adam,
+        'SGD': model_sgd,
+        'RMSprop': model_rmsprop
+    }
 
-get_custom_objects().update({'swish': swish})
+model_dict = load_models()
 
-# Muat model
-model = tf.keras.models.load_model('model_Adam.h5')
+# Judul Aplikasi
+st.title('Klasifikasi Kue Indonesia')
 
-# ... kode selanjutnya tetap sama ...
-# Daftar label kelas
-kelas = [
-    'Kue Coklat',
-    'Kue Keju',
-    'Kue Stroberi',
-    'Kue Lapis',
-    'Kue Brownies',
-    'Kue Nastar',
-    'Kue Putu',
-    'Kue Apem'
-]
-
-st.title("Aplikasi Klasifikasi Citra Digital Kue dengan EfficientNetB0")
-st.write("Unggah gambar kue untuk diklasifikasikan ke dalam salah satu dari 8 kelas.")
+# Pilih optimizer yang ingin digunakan
+optimizer_choice = st.selectbox('Pilih optimizer untuk prediksi:', ['Adam', 'SGD', 'RMSprop'])
 
 # Upload gambar
-uploaded_file = st.file_uploader("Pilih gambar", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Unggah gambar kue", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Baca gambar
-    image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Gambar yang diunggah', use_column_width=True)
-    
-    # Resize sesuai input model (misalnya 224x224 untuk EfficientNetB0)
-    image_resized = image.resize((224, 224))
-    
-    # Konversi ke array numpy
-    img_array = np.array(image_resized)
-    
-    # Preprocessing sesuai EfficientNetB0
-    img_preprocessed = preprocess_input(img_array)
-    
-    # Tambahkan batch dimension
-    img_input = np.expand_dims(img_preprocessed, axis=0)
-    
+    # Tampilkan gambar yang diunggah
+    image = load_img(uploaded_file, target_size=(224, 224))
+    st.image(image, caption='Gambar Unggahan', use_column_width=True)
+
+    # Konversi gambar ke array dan preprocess
+    img_array = img_to_array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
     # Prediksi
-    pred = model.predict(img_input)
-    pred_kelas = np.argmax(pred, axis=1)[0]
-    confidence = pred[0][pred_kelas]
-    
-    # Tampilkan hasil
-    st.write(f"Prediksi: **{kelas[pred_kelas]}**")
-    st.write(f"Kepercayaan: {confidence*100:.2f}%")
+    model = model_dict[optimizer_choice]
+    pred_probs = model.predict(img_array)
+    pred_class = np.argmax(pred_probs, axis=1)[0]
+
+    # Tampilkan hasil prediksi
+    class_labels = list(test_generator.class_indices.keys())  # Pastikan ini sesuai dengan data
+    predicted_label = class_labels[pred_class]
+    confidence = pred_probs[0][pred_class]
+
+    st.write(f'Prediksi: **{predicted_label}** dengan kepercayaan {confidence:.2f}')
