a91f8a7eda45977991a30063a87a61cd14dbdc83
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-17 07:36:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import load_model
 
 # Muat model yang sudah disimpan
 model_path = 'final_model_Adam.h5'  # Ganti sesuai file model Anda
-model = tf.keras.models.load_model(model_path, compile=False)
+model = (model_path)
 
 # Daftar kelas yang digunakan selama pelatihan
 # Pastikan sesuai dengan class_indices dari data generator Anda
